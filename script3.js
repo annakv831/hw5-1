516892d2342825b4af68f15f9c72879d2442cb64
annakv831 <annakoley1000@gmail.com>
2021-11-08 20:40:53 +0200

    add change2 script3

--- script3.js
+++ script3.js
@@ -1,21 +1,12 @@
 function showPrimes(n,m){
-    if (n==1) {
-    metka: for( let i=n+1; i<=m; i++) {
+    
+    metka: for( let i=n>=2?n:2; i<=m; i++) {
         for(let j=2; j<i; j++){
             if(i%j==0) continue metka;
             }
         console.log(i);
-    }} 
-    else {
-        metka2: for(let i=n; i<=m; i++){
-                    for(let j=2; j<i; j++){
-                        if(i%j==0) continue metka2;
-
-                    }
-                console.log(i);
-                }
-         }    
-
+    }
+   
 }
 
 
