e3296d240e1df5fbd28dd160316b9b8b6ac664c0
annakv831 <annakoley1000@gmail.com>
2021-11-08 20:20:47 +0200

    add change script3

--- script3.js
+++ script3.js
@@ -1,14 +1,25 @@
 function showPrimes(n,m){
- metka:for(let i=n; i<=m; i++){
+    if (n==1) {
+    metka: for( let i=n+1; i<=m; i++) {
         for(let j=2; j<i; j++){
             if(i%j==0) continue metka;
-
-        }
+            }
         console.log(i);
-    }
+    }} 
+    else {
+        metka2: for(let i=n; i<=m; i++){
+                    for(let j=2; j<i; j++){
+                        if(i%j==0) continue metka2;
+
+                    }
+                console.log(i);
+                }
+         }    
 
 }
-const startNumber = +prompt("Enter number:");
-const finishNumber = +prompt("Enter number:");
+
+
+const startNumber = +prompt("Enter start number:");
+const finishNumber = +prompt("Enter finish  number:");
 
 showPrimes(startNumber, finishNumber);
